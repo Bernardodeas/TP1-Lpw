8b6998102f97a72d6d119fb4b1a6ae2192227e7f
Calango <bernardodeas2@gmail.com>
2023-06-26 18:48:32 -0300

    mudança mencionada anteriormente

---
+++ script.js
@@ -0,0 +1,6 @@
+function ShowAwns(){
+    var x = document.getElementsByClassName("HideRespostas");
+    var y = document.getElementsByClassName("Resps");
+    x.style.display = "none";
+    y.style.display = "block";
+}
